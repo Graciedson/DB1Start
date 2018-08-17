89b3c4ad1d0e20280078c2643f84b8abfcebbdda
Graciedson <graciedson27@gmail.com>
2018-08-17 18:02:06 -0300

    Commit

--- src/test/java/br/com/db1/ExercicioDeColecaoTest.java
+++ src/test/java/br/com/db1/ExercicioDeColecaoTest.java
@@ -1,5 +1,6 @@
 package br.com.db1;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.List;
@@ -26,9 +27,31 @@ public class ExercicioDeColecaoTest {
 	@Test
 	public void exercicio3AddTest() {
 		List<String> nome = exercicio.exercicio3Add();
-		assertTrue(nome.get(0) == "Antonio");
-		assertTrue(nome.get(1) == "Zeny");
-		assertTrue(nome.get(3) == "Claudia");
+		assertFalse(nome.contains("Antonio"));
+		assertFalse(nome.contains("Zeny"));
 
 	}
+
+	@Test
+	public void exercicio4CidadeNatalTest() {
+		List<String> cidade = exercicio.exercicio4CidadeNatal();
+		assertFalse(cidade.contains("Itapevi"));
+	}
+
+	@Test
+	public void exercicio5ClassificaCorTest() {
+
+	List<String> cor = exercicio.exercicio5ClassificaCor();
+
+	assertTrue(cor.get(0) == "Amarelo");
+	assertTrue(cor.get(1) == "Branco");
+	assertTrue(cor.get(2) == "Preto");
+	assertTrue(cor.get(3) == "Verde");
+}
+	/*
+	@Test
+	public void exercicio6EliminaCorTest();
+	List<String> cor = exercicio.exercicio6EliminaCor();
+	assertTrue(cor.get(1) == "Branco");
+	*/
 }
